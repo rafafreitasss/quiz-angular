0cec2690d21906225f49d355e7a67c32ea356e9e
rafafreitasss <rafaelfreitas97@yahoo.com>
2024-06-19 11:21:00 -0300

    add mq na branch main

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -4,7 +4,7 @@ import quizz_questions from '../../../assets/data/quizz_questions.json'
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css', './quizz.component.mq.css']
 })
 export class QuizzComponent implements OnInit {
   
